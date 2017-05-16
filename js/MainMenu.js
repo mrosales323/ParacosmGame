65c0caed7b0f2ba64765aa1c3a8d403da8546af3
JoshuaNavarro <joanavar@ucsc.edu>
2017-05-16 16:17:45 -0700

    Add files via upload

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -1,6 +1,14 @@
 
 var PlayButton;
+var PlayButtonText;
+
+
+var QuitButton;
+var QuitButtonText;
+
+
 var cursor;
+
 
 
 var MainMenu=function(game){
@@ -15,13 +23,26 @@ console.log("Loading Main Menu Assets. Yum");
 MainMenu.prototype.create=function(){
  
   // PlayButton= game.add.button(game.world.centerX - 95, 400, 'PlayButton', actionOnClick, this, 2, 1, 0);
- var PlayButton = game.add.sprite(game.world.centerX, game.world.centerY, 'PlayButton');
+    PlayButton = game.add.sprite(game.world.centerX, game.world.centerY, 'BlankButton');
+
+    var PlayButtonStyle = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: PlayButton.width, align: "center" };
+    PlayButtonText = game.add.text(PlayButton.centerX*.95, PlayButton.centerY*.95, "Play", PlayButtonStyle);
+  
+
+
+    QuitButton = game.add.sprite(game.world.centerX, game.world.centerY*1.5, 'BlankButton');
+
+    var QuitButtonStyle = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: PlayButton.width, align: "center" };
+    QuitButtonText = game.add.text(QuitButton.centerX*.95, QuitButton.centerY*.95, "Quit", QuitButtonStyle);
+
+//PlayButtonText=game.add.text(game.camera.x+game.width-185,game.camera.y+game.height-240,{fill:"#abcdef"});
+ // PlayButtonText.anchor.set(0.5);
   // PlayButton.onInputOver.add(over, this);
   PlayButton.inputEnabled=true;
   PlayButton.events.onInputDown.add(actionOnClick, this);
 
 cursor= game.add.sprite(0,0,"cursor");
-cursor.scale.setTo(15,15);
+cursor.scale.setTo(8,8);
 //cursor.anchor(0.5,0.5);
 cursor.smoothed=false;
 console.log("HELP ME");
